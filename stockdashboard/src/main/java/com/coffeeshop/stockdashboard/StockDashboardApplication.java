835e1a63855a136fe83e6d3f8e466b1dcd434547
Johny Ramos <B00139596@mytudublin.ie>
2024-02-05 23:26:31 +0000

    Eighth Commit. Changes in buttons.

--- stockdashboard/src/main/java/com/coffeeshop/stockdashboard/StockDashboardApplication.java
+++ stockdashboard/src/main/java/com/coffeeshop/stockdashboard/StockDashboardApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class StockdashboardApplication {
+public class StockDashboardApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(StockdashboardApplication.class, args);
+		SpringApplication.run(StockDashboardApplication.class, args);
 	}
 
 }
